2f21d7739eeefb157bf8d79ad410703a4bb82ff9
Asad <asadnizami123@gmail.com>
2021-04-16 20:42:22 +0530

    'day4'

--- project/settings.py
+++ project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
